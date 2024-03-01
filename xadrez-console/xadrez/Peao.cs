6a72f540754011951ab722e4429d9783ce9dc548
Felipe Mauri <felipe-mauri@hotmail.com>
2024-02-29 22:53:57 -0300

    jogada especial roque pequeno e roque pequeno

--- xadrez-console/xadrez/Peao.cs
+++ xadrez-console/xadrez/Peao.cs
@@ -16,7 +16,7 @@ namespace xadrez
 
         private bool existeInimigo(Posicao pos)
         {
-            Peca p = tab.peca(pos);
+            Peca p = tab.peca(pos); 
             return p != null && p.cor != cor;
         }
 
@@ -44,12 +44,12 @@ namespace xadrez
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna -1);
-                if (tab.posicaoValida(pos) && livre(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha - 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
@@ -67,12 +67,12 @@ namespace xadrez
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna - 1);
-                if (tab.posicaoValida(pos) && livre(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
                 pos.definirValores(posicao.linha + 1, posicao.coluna + 1);
-                if (tab.posicaoValida(pos) && livre(pos))
+                if (tab.posicaoValida(pos) && existeInimigo(pos))
                 {
                     mat[pos.linha, pos.coluna] = true;
                 }
